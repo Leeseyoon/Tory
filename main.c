cc66ff90f9f2988161d8df85fa8b714d673c48e8
Leeseyoon <custmor@kookmin.ac.kr>
2020-11-13 20:30:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -28,26 +28,35 @@ int main()
     printf("print(\"Addition\")\n");
     
     //n번 덧셈 연산하기
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < 20; i++) // n에 20 대입
     {
-        size1 = (rand() & 7) + 1;
-        size2 = (rand() & 7) + 1;
+        size1 = (rand() & 7) + 1; // size1 : 1~8
+        size2 = (rand() & 7) + 1; // size2 : 1~8
 
-        BI_New(&bi_1, size1);
-        BI_New(&bi_2, size2);
-        size = Compare_WordLen(bi_1, bi_2);
-        BI_New(&bi_re, size + 1);
-        bi_1->sign = rand() & 1;
-        bi_2->sign = rand() & 1;
+        BI_New(&bi_1, size1); // size1 길이인 big integer인 bi_1 생성
+        BI_New(&bi_2, size2); // size2 길이인 big integer인 bi_2 생성
+        size = Compare_WordLen(bi_1, bi_2); // size1 과 size2 비교 // Compare_WordLen 보다 MAX 함수 쓰는게 더 낫지 않나??(세윤)
+        BI_New(&bi_re, size + 1); // 더 긴 길이인 size로 덧셈 연산 결과인 big integer 생성
+        bi_1->sign = rand() & 1; // bi_1 의 부호를 랜덤하게
+        bi_2->sign = rand() & 1; // bi_2 의 부호를 랜덤하게
 
         for (j = 0; j < size1; j++)
-            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & word_mask); //bi_1의 값을 랜덤하게 생성 //WORD_BIT_LEN에 따른 rand() 제곱의 값 바꾸기(세윤)
         for (j = 0; j < size2; j++)
-            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & word_mask); //bi_2의 값을 랜덤하게 생성 //WORD_BIT_LEN에 따른 rand() 제곱의 값 바꾸기(세윤)
 
 
-        ADD_BI_test(&bi_re, &bi_1, &bi_2);
+        ADD(&bi_re, &bi_1, &bi_2); // ADD함수에 덧셈 결과인 bi_re , 피연산1인 bi_1, 피연산자2인 bi_2
+        
+        // 출력 부분
+        printf("A = ");
+        BI_Show(bi_1, 16);
+        printf("B = ");
+        BI_Show(bi_2, 16);
+        printf("A + B == ");
+        BI_Show(bi_re, 16);
         printf("\n");
+        // 메모리 free
         BI_Delete(&bi_1);
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
@@ -55,26 +64,34 @@ int main()
     
     printf("print(\"Subtraction\")\n");
     //n번 뺄셈 연산하기
-    for (i = 0; i < 20; i++) 
+    for (i = 0; i < 20; i++) // n에 20 대입
     {
-        size1 = (rand() & 7) + 1;
-        size2 = (rand() & 7) + 1;
+        size1 = (rand() & 7) + 1; // size1 : 1~8
+        size2 = (rand() & 7) + 1; // size2 : 1~8
         
-        size = (size1 > size2) ? size1 : size2;
-        BI_New(&bi_1, size1);
-        BI_New(&bi_2, size2);
-        BI_New(&bi_re, size + 1);
+        size = (size1 > size2) ? size1 : size2; // size1 과 size2 비교해서 size 변수에 대입
+        BI_New(&bi_1, size1); // size1 길이인 big integer인 bi_1 생성
+        BI_New(&bi_2, size2); // size2 길이인 big integer인 bi_2 생성
+        BI_New(&bi_re, size + 1); //size + 1?
         
         bi_1->sign = rand() & 1;
         bi_2->sign = rand() & 1;
 
+        
         for (j = 0; j < size1; j++)
-            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & word_mask);
         for (j = 0; j < size2; j++)
-            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & word_mask);
      
        
-        SUB_BI_test(&bi_re, bi_1, bi_2);
+        SUB(&bi_re, bi_1, bi_2);
+
+        printf("A = ");
+        BI_Show(bi_1, 16);
+        printf("B = ");
+        BI_Show(bi_2, 16);
+        printf("A - B == ");
+        BI_Show(bi_re, 16);
         printf("\n");
         BI_Delete(&bi_1);
         BI_Delete(&bi_2);
@@ -94,48 +111,52 @@ int main()
         bi_2->sign = rand() & 1;
         
         for (j = 0; j < size1; j++)
-            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff); //rand() & (char)0xff; //
+            bi_1->a[j] = ((rand() * rand() * rand() * rand()) & word_mask); //rand() & (char)0xff; //
         for (j = 0; j < size2; j++)
-            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & 0xffffffff); //rand() & (char)0xff; //
+            bi_2->a[j] = ((rand() * rand() * rand() * rand()) & word_mask); //rand() & (char)0xff; //
             
         MUL_MUL(&bi_re, bi_1, bi_2);
+        printf("A = ");
+        BI_Show(bi_1, 16);
+        printf("B = ");
+        BI_Show(bi_2, 16);
+        printf("A * B == ");
+        BI_Show(bi_re, 16);
         printf("\n");
         BI_Delete(&bi_1);
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
     }
     
-    printf("print(\"Karatsuba Multiplication\")\n");
-    for (i = 0; i < 20; i++) //20번 카라추바 곱셈 연산하기
-    {
-        printf("i = %d\n", i);
-        BI_New(&bi_1, size1);
-        BI_New(&bi_2, size2);
-        BI_New(&bi_re, size1 + size2);
+    //printf("print(\"Karatsuba Multiplication\")\n");
+    //for (i = 0; i < 20; i++) //20번 카라추바 곱셈 연산하기
+    //{
+    //    printf("i = %d\n", i);
+    //    BI_New(&bi_1, 4);
+    //    BI_New(&bi_2, 4);
+    //    BI_New(&bi_re, 8);
 
-        bi_1->sign = rand() & 1;
-        bi_2->sign = rand() & 1;
+    //    bi_1->sign = rand() & 1;
+    //    bi_2->sign = rand() & 1;
 
-        bi_1->a[0] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_1->a[1] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_1->a[2] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_1->a[3] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_1->a[4] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_2->a[0] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_2->a[1] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_2->a[2] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
-        bi_2->a[3] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_1->a[0] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_1->a[1] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_1->a[2] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_1->a[3] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //   
+    //    bi_2->a[0] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_2->a[1] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_2->a[2] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
+    //    bi_2->a[3] = ((rand() * rand() * rand() * rand()) & 0xffffffff);
 
-        Karatsuba(&bi_re, bi_1, bi_2);
-        printf("\n");
+    //    Karatsuba(&bi_re, bi_1, bi_2);
+    //    printf("\n");
 
-        BI_Delete(&bi_1);
-        BI_Delete(&bi_2);
-        BI_Delete(&bi_re);
-    }
-    
-    
+    //    BI_Delete(&bi_1);
+    //    BI_Delete(&bi_2);
+    //    BI_Delete(&bi_re);
+    //}
+       
     _CrtDumpMemoryLeaks();
-
     return 0;
 }
