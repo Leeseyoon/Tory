e1398a7ae90ba841b15d8dd67a4277a75c098467
Leeseyoon <custmor@kookmin.ac.kr>
2020-11-26 23:12:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -29,13 +29,17 @@ int main()
     bigint* bi_re = NULL;
     bigint* bi_q = NULL;
 
+    printf("print(\"WORD_BIT_LEN == %d\")\n", WORD_BIT_LEN);
 
+    /* n번 덧셈 연산하기 */ // 8, 32, 64
+    
+    printf("\n\n");
     printf("print(\"Addition\")\n");
-    //n번 덧셈 연산하기
+    
     for (i = 0; i < 20; i++) // n에 20 대입
     {
-        size1 = (rand() & 0xf) + 1; // size1 : 1~16
-        size2 = (rand() & 0xf) + 1; // size2 : 1~16
+        size1 = (rand() & 0x3) + 1; // size1 : 1~16
+        size2 = (rand() & 0x3) + 1; // size2 : 1~16
 
         bi_gen_rand(&bi_1, rand() & 1, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
         bi_gen_rand(&bi_2, rand() & 1, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
@@ -60,14 +64,16 @@ int main()
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
     }
-
-
+    
+    /* n번 뺄셈 연산하기 */ // 8, 32, 64
+    
+    printf("\n");
     printf("print(\"Subtraction\")\n");
-    //n번 뺄셈 연산하기
+    
     for (i = 0; i < 20; i++) // n에 20 대입
     {
-        size1 = (rand() & 0xf) + 1; // size1 : 1~16
-        size2 = (rand() & 0xf) + 1; // size2 : 1~16
+        size1 = (rand() & 0x7) + 1; // size1 : 1~16
+        size2 = (rand() & 0x7) + 1; // size2 : 1~16
 
         bi_gen_rand(&bi_1, rand() & 1, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
         bi_gen_rand(&bi_2, rand() & 1, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
@@ -89,13 +95,16 @@ int main()
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
     }
+     
 
-
+    /* n번 Schoolbook 곱셈 연산하기 */ // 8, 32, 64
+    
+    printf("\n\n");
     printf("print(\"Schoolbook Multiplication\")\n");
     for (i = 0; i < 20; i++) //n번 곱셈 연산하기
     {
-        size1 = (rand() & 0xf) + 1; // size1 : 1~16
-        size2 = (rand() & 0xf) + 1; // size2 : 1~16
+        size1 = (rand() & 0x7) + 1; // size1 : 1~16
+        size2 = (rand() & 0x7) + 1; // size2 : 1~16
 
         bi_gen_rand(&bi_1, rand() & 1, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
         bi_gen_rand(&bi_2, rand() & 1, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
@@ -116,16 +125,110 @@ int main()
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
     }
-
-    // ************************ 메모리 릭 아주 많이 남 ㅎㅎ *********************
-
     
-    printf("print(\"Karatsuba Multiplication\")\n");
 
-    for (i = 0; i < 20; i++) //20번 카라추바 곱셈 연산하기
+    /* n번 Binary Long Division 연산하기 */ // 8, 32, 64
+    
+    printf("\n");
+    printf("print(\"Binary Long Division\")\n"); // 메모리 릭 잡음
+    for (i = 0; i < 20; i++)
     {
-        size1 = (rand() & 0xf) + 1;
-        size2 = (rand() & 0xf) + 1;
+        size2 = (rand() & 3) + 1; // size2 : 1~4
+        size1 = size2 + (rand() & 3) + 1; // size1은 size2 보다 길이 더 길게
+
+
+        BI_New(&bi_1, size1); // size1 길이인 big integer인 bi_1 생성
+        BI_New(&bi_2, size2); // size2 길이인 big integer인 bi_2 생성
+
+        bi_gen_rand(&bi_1, 0, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
+        bi_gen_rand(&bi_2, 0, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
+
+        size = Compare_BI(&bi_1, &bi_2); // size1 과 size2 비교
+        if (size == -1)
+        {
+            printf("print(\"B is larger than A\")\n");
+            BI_Delete(&bi_1);
+            BI_Delete(&bi_2);
+        }
+        else
+        {
+            Binary_Long_Div(&bi_q, &bi_re, bi_1, bi_2);
+            // 출력 부분
+            printf("A = ");
+            BI_Show(bi_1, 16);
+            printf("B = ");
+            BI_Show(bi_2, 16);
+            printf("Q = ");
+            BI_Show(bi_q, 16);
+            printf("R = ");
+            BI_Show(bi_re, 16);
+            printf("A == B * Q + R");
+            printf("\n");
+            // 메모리 free
+            BI_Delete(&bi_1);
+            BI_Delete(&bi_2);
+            BI_Delete(&bi_re);
+            BI_Delete(&bi_q);
+        }
+    }
+    
+
+    /* n번 Multi Long Division 연산하기 */ // 32, 64 (8이고 size가 클 경우 몇 개 False)
+    
+    printf("\n");
+    printf("print(\"Multi Long Division\")\n");
+    for (i = 0; i < 20; i++)
+    {
+        size2 = (rand() & 0xf) + 20; // size2 : 1~4
+        size1 = size2 + (rand() & 0xf);//size2 + (rand() & 3) + 1; // size1은 size2 보다 길이 더 길게
+
+        BI_New(&bi_1, size1); // size1 길이인 big integer인 bi_1 생성
+        BI_New(&bi_2, size2); // size2 길이인 big integer인 bi_2 생성
+             
+
+        bi_gen_rand(&bi_1, 0, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
+        bi_gen_rand(&bi_2, 0, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
+
+        size = Compare_BI(&bi_1, &bi_2); // size1 과 size2 비교
+        if (size == -1)
+        {
+            printf("print(\"B is larger than A\")\n");
+            BI_Delete(&bi_1);
+            BI_Delete(&bi_2);
+        }
+        else
+        {
+            DIV(&bi_q, &bi_re, bi_1, bi_2);
+            // 출력 부분
+            printf("A = ");
+            BI_Show(bi_1, 16);
+            printf("B = ");
+            BI_Show(bi_2, 16);
+            printf("Q = ");
+            BI_Show(bi_q, 16);
+            printf("R = ");
+            BI_Show(bi_re, 16);
+            printf("A == B * Q + R");
+            printf("\n");
+            // 메모리 free
+            BI_Delete(&bi_1);
+            BI_Delete(&bi_2);
+            BI_Delete(&bi_re);
+            BI_Delete(&bi_q);
+        }
+    }
+    
+    
+    
+
+    /* n번 Karatsuba 곱셈 연산하기 */ // 8, 32, 64
+    
+    printf("\n\n");
+    printf("print(\"Karatsuba Multiplication\")\n");
+    for (i = 0; i < 1; i++) //20번 카라추바 곱셈 연산하기
+    {
+        size1 = (rand() & 0x3) + 100;
+        size2 = (rand() & 0x3) + 100;
 
         bi_gen_rand(&bi_1, rand() & 1, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
         bi_gen_rand(&bi_2, rand() & 1, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
@@ -155,7 +258,7 @@ int main()
 
         //bi_re = Kara(bi_1, bi_2);
         Karatsuba(&bi_re, bi_1, bi_2);
-        bi_refine(bi_re);
+        BI_Refine(bi_re);
 
         if ((sign_bi_1 ^ sign_bi_2) == 1)
             bi_re->sign = NEGATIVE;
@@ -171,12 +274,15 @@ int main()
         BI_Delete(&bi_2);
         BI_Delete(&bi_re);
     }
+    
 
+    /* n번 Squaring 곱셈 연산하기 */ // 32, 64 (8일 때 메모리 충돌)
+    
+    printf("\n");
     printf("print(\"Squaring\")\n");
-
-    for (i = 0; i < 20; i++) //20번 squaring
+    for (i = 0; i < 1; i++) //20번 squaring
     {
-        size1 = (rand() & 0xf) + 1;
+        size1 = (rand() & 0xf) + 100;
 
         bi_gen_rand(&bi_1, rand() & 1, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
 
@@ -186,7 +292,7 @@ int main()
         BI_New(&bi_re, size1 * 2 + 1);
 
         Multi_Squaring(bi_re, bi_1);
-        bi_refine(bi_re);
+        BI_Refine(bi_re);
 
         printf("A * A == ");
         BI_Show(bi_re, 16);
@@ -196,51 +302,10 @@ int main()
         BI_Delete(&bi_1);
         BI_Delete(&bi_re);
     }
-
-    printf("print(\"Binary Long Division\")\n");
-    for (i = 0; i < 20; i++)
-    {
-        size2 = (rand() & 3) + 1; // size2 : 1~4
-        size1 = size2 + (rand() & 3) + 1; // size1은 size2 보다 길이 더 길게
-
-
-        BI_New(&bi_1, size1); // size1 길이인 big integer인 bi_1 생성
-        BI_New(&bi_2, size2); // size2 길이인 big integer인 bi_2 생성
-
-        bi_gen_rand(&bi_1, 0, size1); // bi_1 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
-        bi_gen_rand(&bi_2, 0, size2); // bi_2 의 부호를 랜덤하게 만들어서 랜덤한 배열을 담은 bigint 생성
-
-        size = Compare_BI(&bi_1, &bi_2); // size1 과 size2 비교
-        if (size == -1)
-        {
-            printf("print(\"B is larger than A\")\n");
-            BI_Delete(&bi_1);
-            BI_Delete(&bi_2);
-        }
-        else
-        {
-
-            Binary_Long_Div(&bi_q, &bi_re, bi_1, bi_2);
-            // 출력 부분
-            printf("A = ");
-            BI_Show(bi_1, 16);
-            printf("B = ");
-            BI_Show(bi_2, 16);
-            printf("Q = ");
-            BI_Show(bi_q, 16);
-            printf("R = ");
-            BI_Show(bi_re, 16);
-            printf("A == B * Q + R");
-            printf("\n");
-            // 메모리 free
-            BI_Delete(&bi_1);
-            BI_Delete(&bi_2);
-            BI_Delete(&bi_re);
-            BI_Delete(&bi_q);
-        }
-    }
     
-
+    
+    
+    
     _CrtDumpMemoryLeaks();
     return 0;
 }
