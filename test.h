62d44c9b8f88ea416041a9c6395f7d69d3239344
JungSeowoo <73089110+JungSeowoo@users.noreply.github.com>
2020-11-30 01:12:58 +0900

    Add files via upload

---
+++ test.h
@@ -0,0 +1,21 @@
+#ifndef _TEST_
+#define _TEST
+
+int addition_test();
+int subtraction_test();
+
+int schoolbook_multiplication_test();
+int karatsuba_multiplication_test();
+
+int binary_long_division_test();
+int multi_long_division_test();
+
+int squaring_test();
+int squaring_karatsuba_test();
+
+int montgomery_Exponentiation_multiplication_test();
+int montgomery_ladder_Exponentiation_addtion_test();
+int montgomery_ladder_Modular_Exponentiation_multiplication_test();
+int montgomery_ladder_Modular_Exponentiation_addtion_test();
+
+#endif
