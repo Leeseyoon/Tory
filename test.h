d8511f86c138cda06adc4c7dbe8b8080e087d5d8
Leeseyoon <custmor@kookmin.ac.kr>
2020-11-30 01:50:42 +0900

    Update test.h

--- test.h
+++ test.h
@@ -18,4 +18,17 @@ int montgomery_ladder_Exponentiation_addtion_test();
 int montgomery_ladder_Modular_Exponentiation_multiplication_test();
 int montgomery_ladder_Modular_Exponentiation_addtion_test();
 
+void Left_Right_EXP_ADD_test(); // Test of 'Only Exponentiation function L->R +'
+void Left_Right_EXP_MUL_test(); // Test of 'Only Exponentiation function L->R *'
+void Right_Left_EXP_ADD_test(); // Test of 'Only Exponentiation function L<-R +'
+void Right_Left_EXP_MUL_test(); // Test of 'Only Exponentiation function L<-R *'
+void Left_Right_EXP_ADD_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L->R +'
+void Left_Right_EXP_MUL_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L->R *'
+void Right_Left_EXP_ADD_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R +'
+void Right_Left_EXP_MUL_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R *'
+void Left_Right_EXP_ADD_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L->R +'
+void Left_Right_EXP_MUL_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L->R *'
+void Right_Left_EXP_ADD_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L<-R +'
+void Right_Left_EXP_MUL_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L<-R *'
+
 #endif
