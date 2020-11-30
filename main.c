d8ed039544338d17368cd9450ac7b61015a1faa7
Leeseyoon <custmor@kookmin.ac.kr>
2020-12-01 00:33:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "bigint.h"
+#include "test.h"
 
 #ifndef _DEBUG
 #define new new(_CLIENT_BLOCK, __FILE__, __LINE)
@@ -15,40 +16,34 @@
 
 int main()
 {
-    srand((unsigned)time(NULL));
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    //_crtBreakAlloc = 94;
-    
-    /* test */
-#if 0
-    addition_test();
-    subtraction_test();
-    schoolbook_multiplication_test();
-    karatsuba_multiplication_test();
-    binary_long_division_test();
-    multi_long_division_test();
-    squaring_test();
-    //squaring_karatsuba_test();
-    montgomery_Exponentiation_multiplication_test();
-    montgomery_ladder_Exponentiation_addtion_test();
-    montgomery_ladder_Modular_Exponentiation_multiplication_test();
-    montgomery_ladder_Modular_Exponentiation_addtion_test();
-    Left_Right_EXP_ADD_test(); 
-	Left_Right_EXP_MUL_test();
-	Right_Left_EXP_ADD_test();
-	Right_Left_EXP_MUL_test(); // Only Exponentiation
-	Left_Right_EXP_ADD_MOD_test();
-	Left_Right_EXP_MUL_MOD_test();
-	Right_Left_EXP_ADD_MOD_test();
-	Right_Left_EXP_MUL_MOD_test(); // Modular Exponentiaition used Binary Long Division
-	Left_Right_EXP_ADD_MOD_DIV_test();
-	Left_Right_EXP_MUL_MOD_DIV_test();
-	Right_Left_EXP_ADD_MOD_DIV_test();
-	Right_Left_EXP_MUL_MOD_DIV_test(); // Modular Exponentiation used Multi Long Division
-#endif    
-    squaring_karatsuba_test();
-    //squaring_test();
-    
-    _CrtDumpMemoryLeaks();
-    return 0;
+	srand((unsigned)time(NULL));
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_crtBreakAlloc = 94;
+	addition_test(); // Memory 0
+	subtraction_test(); // Memory 0
+	schoolbook_multiplication_test(); // Memory 0
+	karatsuba_multiplication_test();
+	binary_long_division_test(); // Memory 0
+	multi_long_division_test(); // Memory 0
+	squaring_test(); // Memory 0
+	//squaring_karatsuba_test();
+	montgomery_Exponentiation_multiplication_test(); // Memory 0
+	montgomery_ladder_Exponentiation_addtion_test(); // Memory 0
+	montgomery_ladder_Modular_Exponentiation_multiplication_test(); // Memory 0
+	//montgomery_ladder_Modular_Exponentiation_addtion_test(); 
+	Left_Right_EXP_ADD_test(); // Memory 0
+	//Left_Right_EXP_MUL_test();
+	Right_Left_EXP_ADD_test(); // Memory 0
+	Right_Left_EXP_MUL_test(); // Memory 0
+	Left_Right_EXP_ADD_MOD_test(); // Memory 0
+	//Left_Right_EXP_MUL_M3OD_test();
+	Right_Left_EXP_ADD_MOD_test(); // Memory 0
+	Right_Left_EXP_MUL_MOD_test(); // Memory 0
+	//Left_Right_EXP_ADD_MOD_DIV_test();
+	//Left_Right_EXP_MUL_MOD_DIV_test();
+	//Right_Left_EXP_ADD_MOD_DIV_test();
+	//Right_Left_EXP_MUL_MOD_DIV_test();
+	
+	_CrtDumpMemoryLeaks();
+	return 0;
 }
