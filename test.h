2412b2acabcfb374d29524ff33852808ecffeafe
Leeseyoon <custmor@kookmin.ac.kr>
2020-12-01 13:22:08 +0900

    Update test.h

--- test.h
+++ test.h
@@ -22,13 +22,46 @@ void Left_Right_EXP_ADD_test(); // Test of 'Only Exponentiation function L->R +'
 void Left_Right_EXP_MUL_test(); // Test of 'Only Exponentiation function L->R *'
 void Right_Left_EXP_ADD_test(); // Test of 'Only Exponentiation function L<-R +'
 void Right_Left_EXP_MUL_test(); // Test of 'Only Exponentiation function L<-R *'
+
 void Left_Right_EXP_ADD_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L->R +'
 void Left_Right_EXP_MUL_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L->R *'
 void Right_Left_EXP_ADD_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R +'
 void Right_Left_EXP_MUL_MOD_test(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R *'
+
 void Left_Right_EXP_ADD_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L->R +'
 void Left_Right_EXP_MUL_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L->R *'
 void Right_Left_EXP_ADD_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L<-R +'
 void Right_Left_EXP_MUL_MOD_DIV_test(); // Test of 'Modular Exponentiation used Multi Long Division L<-R *'
 
+int addition_time();
+int subtraction_time();
+
+int schoolbook_multiplication_time();
+int karatsuba_multiplication_time();
+
+int binary_long_division_time();
+int multi_long_division_time();
+
+int squaring_time();
+int squaring_karatsuba_time();
+
+int montgomery_Exponentiation_multiplication_time();
+int montgomery_ladder_Exponentiation_addtion_time();
+int montgomery_ladder_Modular_Exponentiation_multiplication_time();
+int montgomery_ladder_Modular_Exponentiation_addtion_time();
+
+void Left_Right_EXP_ADD_time(); // Test of 'Only Exponentiation function L->R +'
+void Left_Right_EXP_MUL_time(); // Test of 'Only Exponentiation function L->R *'
+void Right_Left_EXP_ADD_time(); // Test of 'Only Exponentiation function L<-R +'
+void Right_Left_EXP_MUL_time(); // Test of 'Only Exponentiation function L<-R *'
+
+void Left_Right_EXP_ADD_MOD_time(); // Test of 'Modular Exponentiaition used Binary Long Division L->R +'
+void Left_Right_EXP_MUL_MOD_time(); // Test of 'Modular Exponentiaition used Binary Long Division L->R *'
+void Right_Left_EXP_ADD_MOD_time(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R +'
+void Right_Left_EXP_MUL_MOD_time(); // Test of 'Modular Exponentiaition used Binary Long Division L<-R *'
+
+void Left_Right_EXP_ADD_MOD_DIV_time(); // Test of 'Modular Exponentiation used Multi Long Division L->R +'
+void Left_Right_EXP_MUL_MOD_DIV_time(); // Test of 'Modular Exponentiation used Multi Long Division L->R *'
+void Right_Left_EXP_ADD_MOD_DIV_time(); // Test of 'Modular Exponentiation used Multi Long Division L<-R +'
+void Right_Left_EXP_MUL_MOD_DIV_time(); // Test of 'Modular Exponentiation used Multi Long Division L<-R *'
 #endif
