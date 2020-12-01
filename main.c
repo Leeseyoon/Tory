97f015645681c828a07afbc3ddfff42a1ffebb6f
Leeseyoon <custmor@kookmin.ac.kr>
2020-12-01 13:21:05 +0900

    Update main.c

--- main.c
+++ main.c
@@ -18,32 +18,56 @@ int main()
 {
 	srand((unsigned)time(NULL));
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	//_crtBreakAlloc = 94;
-	addition_test(); // Memory 0
-	subtraction_test(); // Memory 0
-	schoolbook_multiplication_test(); // Memory 0
-	karatsuba_multiplication_test(); // Memory 0
-	binary_long_division_test(); // Memory 0
-	multi_long_division_test(); // Memory 0
-	squaring_test(); // Memory 0
-	//squaring_karatsuba_test();
-	montgomery_Exponentiation_multiplication_test(); // Memory 0
-	montgomery_ladder_Exponentiation_addtion_test(); // Memory 0
-	montgomery_ladder_Modular_Exponentiation_multiplication_test(); // Memory 0
-	//montgomery_ladder_Modular_Exponentiation_addtion_test(); // Memory 0
-	Left_Right_EXP_ADD_test(); // Memory 0
+	//_crtBreakAlloc = 93;
+	//addition_test(); // Memory 0
+	//subtraction_test(); // Memory 0
+	//schoolbook_multiplication_test(); // Memory 0
+	//karatsuba_multiplication_test(); // Memory 0
+	//binary_long_division_test(); // Memory 0
+	//multi_long_division_test(); // Memory 0
+	//squaring_test(); // Memory 0
+	////squaring_karatsuba_test();
+	//montgomery_Exponentiation_multiplication_test(); // Memory 0
+	//montgomery_ladder_Exponentiation_addtion_test(); // Memory 0
+	//montgomery_ladder_Modular_Exponentiation_multiplication_test(); // Memory 0
+	//montgomery_ladder_Modular_Exponentiation_addtion_test(); 
+	//Left_Right_EXP_ADD_test(); // Memory 0 
 	//Left_Right_EXP_MUL_test(); // Memory 0
-	Right_Left_EXP_ADD_test(); // Memory 0
-	Right_Left_EXP_MUL_test(); // Memory 0
-	Left_Right_EXP_ADD_MOD_test(); // Memory 0
-	//Left_Right_EXP_MUL_M3OD_test(); // Memory 0
-	Right_Left_EXP_ADD_MOD_test(); // Memory 0
-	Right_Left_EXP_MUL_MOD_test(); // Memory 0
+	//Right_Left_EXP_ADD_test(); // Memory 0
+	//Right_Left_EXP_MUL_test(); // Memory 0
+	//Left_Right_EXP_ADD_MOD_test(); // Memory 0
+	//Left_Right_EXP_MUL_MOD_test(); // 갑자기 안나네? Memory 0
+	//Right_Left_EXP_ADD_MOD_test(); // Memory 0
+	//Right_Left_EXP_MUL_MOD_test(); // Memory 0
 	//Left_Right_EXP_ADD_MOD_DIV_test(); // Memory 0
 	//Left_Right_EXP_MUL_MOD_DIV_test(); // Memory 0
 	//Right_Left_EXP_ADD_MOD_DIV_test(); // Memory 0
 	//Right_Left_EXP_MUL_MOD_DIV_test(); // Memory 0
 	
+	addition_time(); // Memory 0
+	subtraction_time(); // Memory 0
+	schoolbook_multiplication_time(); // Memory 0
+	//karatsuba_multiplication_time(); // Memory 0
+	binary_long_division_time(); // Memory 0
+	multi_long_division_time(); // Memory 0
+	//squaring_time(); // Memory 0
+	////squaring_karatsuba_time();
+	//montgomery_Exponentiation_multiplication_time(); // Memory 0
+	//montgomery_ladder_Exponentiation_addtion_time(); // Memory 0
+	//montgomery_ladder_Modular_Exponentiation_multiplication_time(); // Memory 0
+	//montgomery_ladder_Modular_Exponentiation_addtion_time(); 
+	Left_Right_EXP_ADD_time(); // Memory 0 
+	//Left_Right_EXP_MUL_time(); // Memory 0
+	//Right_Left_EXP_ADD_time(); // Memory 0
+	//Right_Left_EXP_MUL_time(); // Memory 0
+	Left_Right_EXP_ADD_MOD_time(); // Memory 0
+	Left_Right_EXP_MUL_MOD_time(); // 갑자기 안나네? Memory 0
+	Right_Left_EXP_ADD_MOD_time(); // Memory 0
+	Right_Left_EXP_MUL_MOD_time(); // Memory 0
+	Left_Right_EXP_ADD_MOD_DIV_time(); // Memory 0
+	Left_Right_EXP_MUL_MOD_DIV_time(); // Memory 0
+	Right_Left_EXP_ADD_MOD_DIV_time(); // Memory 0
+	Right_Left_EXP_MUL_MOD_DIV_time(); // Memory 0
 	_CrtDumpMemoryLeaks();
 	return 0;
 }
