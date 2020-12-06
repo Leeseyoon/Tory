da720bdc2f31f1359f1d310579acd27285a0d575
JungSeowoo <73089110+JungSeowoo@users.noreply.github.com>
2020-12-07 08:35:42 +0900

    Add files via upload

--- test.h
+++ test.h
@@ -1,21 +1,16 @@
-#ifndef _TEST_
+﻿#ifndef _TEST_
 #define _TEST
 
+/* Test Function */
 int addition_test();
 int subtraction_test();
-
 int multiplication_test();
-
-int division_test();
-
 int squaring_test();
-int squaring_karatsuba_test();
-
+int division_test();
 int modular_exponentiation_multiplication_test();
 int modular_exponentiation_addition_test();
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-
+/* Measuring Time Function */
 int addition_time();
 int subtraction_time();
 
@@ -30,10 +25,15 @@ int squaring_karatsuba_time();
 
 int montgomery_ladder_Modular_Exponentiation_multiplication_time();
 int montgomery_ladder_Modular_Exponentiation_addtion_time();
-
 int Left_Right_EXP_ADD_MOD_time();
 int Left_Right_EXP_MUL_MOD_time();
 int Right_Left_EXP_ADD_MOD_time();
 int Right_Left_EXP_MUL_MOD_time();
+
+/* Compare Time Function */
+int compare_text_kara_mul();
+int compare_text_kara_squ();
+int compare_binary_multi_long_div();
+int compare_mod_exp_Montgomery_LtoR();
 
 #endif
